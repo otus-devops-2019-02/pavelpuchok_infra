ef3eb0959f46e3f2a24c99c4e0bc77fed5f4e4a1
Pavel Puchok <pavel.puchok@gmail.com>
2019-04-20 20:11:42 +0300

    Fix prod main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -9,16 +9,18 @@ provider "google" {
 }
 
 module "app" {
-  source         = "../modules/app"
-  zone           = "${var.zone}"
-  app_disk_image = "${var.app_disk_image}"
-  app_count      = "${var.app_count}"
+  source          = "../modules/app"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
+  app_count       = "${var.app_count}"
+  public_key_path = "${var.public_key_path}"
 }
 
 module "db" {
-  source        = "../modules/db"
-  zone          = "${var.zone}"
-  db_disk_image = "${var.db_disk_image}"
+  source          = "../modules/db"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.db_disk_image}"
+  public_key_path = "${var.public_key_path}"
 }
 
 module "vpc" {
